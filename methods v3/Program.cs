462a92a3790012a7ce5ed453bc5c746daf62a0b1
Yuri Nishioka <115784867+lilyuwu01@users.noreply.github.com>
2025-05-03 16:02:40 +0800

    Add files via upload

---
+++ methods v3/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace methods_v3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintEvenOdd(4);  // Expected output: "Even"
+            PrintEvenOdd(7);  // Expected output: "Odd"
+        }
+
+        public static bool IsEven(int number)
+        {
+            return number % 2 == 0; // Returns true if even, false if odd
+        }
+
+        public static void PrintEvenOdd(int number)
+        {
+            if (IsEven(number)) // Calls IsEven and checks its returned value
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+        }
+    }
+}
+  
+
---
+++ multi-dimensional v2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace multi_dimensional_v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] myArray = new int[3][];
+            myArray[0] = new int[4] { 5, 6, 7, 8 };
+            myArray[1] = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            myArray[2] = new int[2] { 1, 2 };
+
+            for(int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < myArray[i].Length; j++)
+                {
+                    Console.Write(myArray[i][j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
